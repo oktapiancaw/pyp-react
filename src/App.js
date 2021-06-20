808e94488394a8ee48cb3ea8cc3690ff992e811d
oktapiancaw <oktapian1998@gmail.com>
2021-06-20 16:51:58 +0700

    Activate Eslint

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
